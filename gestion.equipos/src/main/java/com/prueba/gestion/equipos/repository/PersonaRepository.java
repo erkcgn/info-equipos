f9fb40bd2727238932212c8495fc2e708b7cc0f4
erika cologne <erikac.ec80@gmail.com>
2024-03-19 19:22:39 -0300

    finalizo logica de usuario pre establecido

--- gestion.equipos/src/main/java/com/prueba/gestion/equipos/repository/PersonaRepository.java
+++ gestion.equipos/src/main/java/com/prueba/gestion/equipos/repository/PersonaRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonaRepository extends JpaRepository<Persona,Long> {
+
+    Persona findByNombre(String nombre);
 }
--- gestion.equipos/src/main/java/com/prueba/gestion/equipos/service/serviceImpl/PersonaServiceImpl.java
+++ gestion.equipos/src/main/java/com/prueba/gestion/equipos/service/serviceImpl/PersonaServiceImpl.java
@@ -1,18 +1,28 @@
 package com.prueba.gestion.equipos.service.serviceImpl;
 
 import com.prueba.gestion.equipos.model.Persona;
+import com.prueba.gestion.equipos.repository.PersonaRepository;
 import com.prueba.gestion.equipos.service.PersonaService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonaServiceImpl implements PersonaService {
 
-    @Override
-    public Persona crearPersona() {
-        Persona objPersona = new Persona();
-        objPersona.setNombre("test");
-        objPersona.setPassword("12345");
+    private final PersonaRepository personaRepository;
 
-        return objPersona;
+    public PersonaServiceImpl(PersonaRepository personaRepository) {
+        this.personaRepository = personaRepository;
+    }
+
+    @Override
+    public void crearPersona() {
+
+        Persona personaExistente = personaRepository.findByNombre("test");
+        if (personaExistente == null) {
+            Persona objPersona = new Persona();
+            objPersona.setNombre("test");
+            objPersona.setPassword("12345");
+            personaRepository.save(objPersona);
+        }
     }
 }
