0024828ddcf22b047ca6bd825c790e262a3f31b7
erika cologne <erikac.ec80@gmail.com>
2024-03-20 09:55:47 -0300

    agrego anotacion pertinente

--- gestion.equipos/src/main/java/com/prueba/gestion/equipos/Application.java
+++ gestion.equipos/src/main/java/com/prueba/gestion/equipos/Application.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class Application {
 	@Bean
 	public PasswordEncoder passwordEncoder(){return new BCryptPasswordEncoder();}
